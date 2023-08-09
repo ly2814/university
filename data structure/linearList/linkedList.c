9cd9034f4c24cc5d3a203250e34a7eff1dd55792
[ly2814] <[3404331682@qq.com]>
2023-08-09 20:12:37 +0800

    Default Changelist

--- data structure/linearList/linkedList.c
+++ data structure/linearList/linkedList.c
@@ -12,15 +12,15 @@ struct ListNode{
 
 typedef struct ListNode * Node;
 
-void initList(Node node){
-    node->next = NULL;
+void initList(Node head){
+    head->next = NULL;
 }
 
 _Bool insertList(Node head, E element, int index){
     if (index < 1)  return 0;
     while (--index){
         head = head->next;
-        if(head->next == NULL) return 0;
+        if(head == NULL) return 0;
     }
     Node node = malloc(sizeof (struct ListNode));
     if(node == NULL)    return 0;
@@ -31,16 +31,18 @@ _Bool insertList(Node head, E element, int index){
 }
 
 void printList(Node head){
-    while (head->next)
+    while (head->next){
         head = head->next;
-        printf("%d",head->element);
+        printf("%d ", head->element);
+    }
 }
 
 int main(){
     struct ListNode head;
     initList(&head);
-    for (int i=0; i<3; i++)
-        insertList(&head, 1*100, i;)
-    printList(&head)
+    for (int i = 1; i <= 3; ++i) {
+        insertList(&head, i*100, i);
+    }
+    printList(&head);
     return 0;
 }
